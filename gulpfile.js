8844b9db5f38c534417411559241619193e2d9f9
cpf <1105823914@qq.com>
2020-06-24 15:42:43 +0800

    init1

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');   //图片压缩
+const uglify = require("gulp-uglify");  //js压缩
+const less = require('gulp-sass');      //sass
+const cleanCSS = require('gulp-clean-css');  //css压缩
+
+ 
+//文件合并
+function copyHtml(){
+    gulp.src("src/pages/*.html")
+        .pipe(gulp.dest("dist"))  //把src目录中所有html格式的文件全部合并到dist目录中
+}
+ 
+//图片压缩
+function imagemin() {
+    gulp.src("src/images/*")   //所有src > images中的图片
+        .pipe(imagemin()) // 图片压缩
+        .pipe(gulp.dest("dist/images"))  //放入到dist目录下面的images文件
+}
+ 
+//js压缩
+function uglify() {
+    gulp.src("src/js/*.js")
+        .pipe(uglify()) //压缩js代码
+        .pipe(gulp.dest("dist/js")) //通过gulp uglify命令，自动输出dist下面js文件
+}
+ 
+//css压缩并less转换成css
+function sass() {
+    gulp.src("src/less/*.sass")  //手动创建
+        .pipe(less()) //将less文件编译成css文件
+        .pipe(cleanCSS())  //压缩css
+        .pipe(gulp.dest("dist/css"))   //通过gulp sass 命令，自动输出dist/css文件
+}
+ 
+//监听文件是否有变化
+gulp.task("default",function(){
+    gulp.watch("src/pages/*.html",copyHtml);
+    gulp.watch("src/images/*",imagemin);
+    gulp.watch("src/js/*.js",uglify);
+    gulp.watch("src/sass/*.sass",sass)
+})
