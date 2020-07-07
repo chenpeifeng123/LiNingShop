c6640a3f2b1c1af05f075373e5debe9c97b7e6a5
cpf <1105823914@qq.com>
2020-07-07 09:59:16 +0800

    上传数据库文件

--- sql/carlist.sql
+++ sql/carlist.sql
@@ -1,39 +1,9 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : login
-Source Server Version : 50617
-Source Host           : localhost:3306
-Source Database       : lining
-
-Target Server Type    : MYSQL
-Target Server Version : 50617
-File Encoding         : 65001
-
-Date: 2020-07-04 08:40:51
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for carlist
--- ----------------------------
-DROP TABLE IF EXISTS `carlist`;
-CREATE TABLE `carlist` (
-  `cart_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(64) COLLATE utf8mb4_bin NOT NULL,
-  `good_id` int(24) NOT NULL,
-  `rule` varchar(24) COLLATE utf8mb4_bin NOT NULL,
-  `num` int(20) NOT NULL,
-  PRIMARY KEY (`cart_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=143 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
--- ----------------------------
--- Records of carlist
--- ----------------------------
-INSERT INTO `carlist` VALUES ('124', 'aaa123', '1003', 'L', '13');
-INSERT INTO `carlist` VALUES ('125', 'aaa123', '1003', 'XXL', '13');
-INSERT INTO `carlist` VALUES ('126', '测试人员', '1005', '3XL', '26');
-INSERT INTO `carlist` VALUES ('141', 'abc123456', '1002', '3XL', '2');
-INSERT INTO `carlist` VALUES ('142', 'abc123456', '1002', 'XL', '1');
-SET FOREIGN_KEY_CHECKS=1;
+INSERT INTO `carlist` VALUES (124, 'aaa123', 1003, 'L', 13);
+INSERT INTO `carlist` VALUES (125, 'aaa123', 1003, 'XXL', 13);
+INSERT INTO `carlist` VALUES (126, '测试人员', 1005, '3XL', 26);
+INSERT INTO `carlist` VALUES (141, 'abc123456', 1002, '3XL', 2);
+INSERT INTO `carlist` VALUES (142, 'abc123456', 1002, 'XL', 1);
+INSERT INTO `carlist` VALUES (150, 'zs123', 1003, 'XL', 13);
+INSERT INTO `carlist` VALUES (151, 'zs123', 1003, 'XXL', 13);
+INSERT INTO `carlist` VALUES (152, 'zs123', 1003, '3XL', 13);
+INSERT INTO `carlist` VALUES (153, 'wangdefa', 1004, '43', 25);
--- sql/details.sql
+++ sql/details.sql
@@ -1,38 +1,5 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : login
-Source Server Version : 50617
-Source Host           : localhost:3306
-Source Database       : lining
-
-Target Server Type    : MYSQL
-Target Server Version : 50617
-File Encoding         : 65001
-
-Date: 2020-07-04 08:41:03
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for details
--- ----------------------------
-DROP TABLE IF EXISTS `details`;
-CREATE TABLE `details` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `minSrc` text NOT NULL,
-  `num` int(11) NOT NULL,
-  `flag` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of details
--- ----------------------------
-INSERT INTO `details` VALUES ('1001', '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_5.jpg\"]', '1', 'cloth');
-INSERT INTO `details` VALUES ('1002', '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_5.jpg\"]', '1', 'cloth');
-INSERT INTO `details` VALUES ('1003', '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_5.jpg\"]', '13', 'cloth');
-INSERT INTO `details` VALUES ('1004', '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_5.jpg\"]', '7', 'shoes');
-INSERT INTO `details` VALUES ('1005', '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_5.jpg\"]', '19', 'cloth');
-SET FOREIGN_KEY_CHECKS=1;
+INSERT INTO `details` VALUES (1001, '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536602/display_536602_5.jpg\"]', 34, 'cloth');
+INSERT INTO `details` VALUES (1002, '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202002/536592/display_536592_5.jpg\"]', 1, 'cloth');
+INSERT INTO `details` VALUES (1003, '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202001/527593/display_527593_5.jpg\"]', 12, 'cloth');
+INSERT INTO `details` VALUES (1004, '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202003/538174/display_538174_5.jpg\"]', 32, 'shoes');
+INSERT INTO `details` VALUES (1005, '[\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_1.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_2.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_3.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_4.jpg\",\"https://cdns.lining.com/postsystem/docroot/images/goods/202004/558543/display_558543_5.jpg\"]', 22, 'cloth');
